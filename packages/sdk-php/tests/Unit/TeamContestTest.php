22715669810fd99773239efc7183dd9ed5532bdc
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2022-08-05 19:20:01 +0200

    feat(sdk-php): prepare test suite for team contests, waiting for real data to test

--- packages/sdk-php/tests/Unit/TeamContestTest.php
+++ packages/sdk-php/tests/Unit/TeamContestTest.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use SmartpingApi\Model\Contest\Team\TeamMatch;
+use SmartpingApi\Model\Contest\Team\TeamMatchDetails;
+use SmartpingApi\Model\Contest\Team\TeamPool;
+use SmartpingApi\Model\Contest\Team\TeamPoolRank;
 use SmartpingApi\SmartpingAPI;
 
 uses()->group('team_contest');
@@ -11,10 +15,32 @@ beforeEach(function() {
     $this->smartping->authenticate();
 });
 
-// it('should return an array of contests', function() {
-//     $list = $this->smartping->findContests(1, ContestType::TEAM);
-//     expect($list)
-//         ->toBeArray()
-//         ->and($list)->not()->toHaveCount(0)
-//         ->and($list[0])->toBeInstanceOf(Contest::class);
-// });
+it('should return an array of pools for a given division of team championship', function() {
+    $list = $this->smartping->getTeamChampionshipPoolsForDivision(20894);
+    expect($list)
+        ->toBeArray()
+        ->and($list)->not()->toHaveCount(0)
+        ->and($list[0])->toBeInstanceOf(TeamPool::class)
+        ->and($list[0]->divisionId())->toBe(20894);
+})->skip();
+
+it('should return an array of matches for a given division and pool of team championship', function() {
+    $list = $this->smartping->getTeamChampionshipMatchesForPool(20894);
+    expect($list)
+        ->toBeArray()
+        ->and($list)->not()->toHaveCount(0)
+        ->and($list[0])->toBeInstanceOf(TeamMatch::class);
+})->skip();
+
+it('should return an array of ranks for a given division and pool of team championship', function() {
+    $list = $this->smartping->getTeamChampionshipPoolRanking(20894);
+    expect($list)
+        ->toBeArray()
+        ->and($list)->not()->toHaveCount(0)
+        ->and($list[0])->toBeInstanceOf(TeamPoolRank::class);
+})->skip();
+
+it('should return a match for a given match id', function() {
+    $result = $this->smartping->getTeamChampionshipMatch(20894);
+    expect($result)->toBeInstanceOf(TeamMatchDetails::class);
+})->skip();
