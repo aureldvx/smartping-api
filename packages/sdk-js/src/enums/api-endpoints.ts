d59d28cde230bd8b62e0a210e41520b873b08a95
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2022-08-07 20:31:46 +0200

    feat(sdk-js): create api endpoints enum

---
+++ packages/sdk-js/src/enums/api-endpoints.ts
@@ -0,0 +1,28 @@
+export enum ApiEndpoints {
+	// Commun
+	XML_INITIALISATION = '/xml_initialisation.php',
+	XML_NEW_ACTU = '/xml_new_actu.php',
+	// Organismes
+	XML_ORGANISME = '/xml_organisme.php',
+	// Clubs
+	XML_CLUB_DEP_2 = '/xml_club_dep2.php',
+	XML_CLUB_B = '/xml_club_b.php',
+	XML_CLUB_DETAIL = '/xml_club_detail.php',
+	// Compétitions
+	XML_EPREUVE = '/xml_epreuve.php',
+	XML_DIVISION = '/xml_division.php',
+	XML_RESULT_EQU = '/xml_result_equ.php',
+	XML_CHP_RENC = '/xml_chp_renc.php',
+	XML_EQUIPE = '/xml_equipe.php',
+	XML_RESULT_INDIV = '/xml_result_indiv.php',
+	XML_RES_CLA = '/xml_res_cla.php',
+	// Joueurs
+	XML_LISTE_JOUEUR = '/xml_liste_joueur.php',
+	XML_LISTE_JOUEUR_O = '/xml_liste_joueur_o.php',
+	XML_JOUEUR = '/xml_joueur.php',
+	XML_LICENCE = '/xml_licence.php',
+	XML_LICENCE_B = '/xml_licence_b.php',
+	XML_PARTIE_MYSQL = '/xml_partie_mysql.php',
+	XML_PARTIE = '/xml_partie.php',
+	XML_HISTO_CLASSEMENT = '/xml_histo_classement.php',
+}
