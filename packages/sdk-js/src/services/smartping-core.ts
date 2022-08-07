7c5f2772ad9dd95235bc1308123af8467e49fea7
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2022-08-07 20:32:57 +0200

    feat(sdk-js): working implementation of the callAPI method

---
+++ packages/sdk-js/src/services/smartping-core.ts
@@ -0,0 +1,174 @@
+import { ApiEndpoints } from '../enums/api-endpoints'
+import dayjs from 'dayjs'
+import { default as Sha } from 'jssha/dist/sha1'
+import md5 from 'md5'
+import { v4 as uuidV4 } from 'uuid'
+import { XMLParser } from 'fast-xml-parser';
+
+// type OrganizationStoreItem = {
+// 	id: number;
+// 	name: string;
+// 	code: string;
+// 	type: string;
+// 	parentId: string;
+// }
+
+type DecodedXml = { [key: string]: string | number | DecodedXml | DecodedXml[] };
+
+export type FetchCallback = (endpoint: string, queryParameters: URLSearchParams) => Promise<string>;
+
+export type FetchProperties<T,P> = {
+	endpoint: ApiEndpoints,
+	requestParameters?: {[key: string]: string},
+	normalizationModel: Newable<T,P>,
+	rootKey: string
+}
+
+export type Newable<T,P> = { new (properties: P): T; };
+
+/** @description Identifiant unique fourni par la FFTT. */
+let kAppId = '';
+
+/** @description Mot de passe unique fourni par la FFTT. */
+let kAppKey = '';
+
+/** @description Chaîne de caractères identifiant l'utilisateur. */
+const kSerial = uuidV4().replaceAll('-', '').slice(0, 15);
+
+/** @description URL de base sur laquelle les endpoints se greffent. */
+const kBaseUrl = 'https://apiv2.fftt.com/mobile/pxml';
+
+/** @description Parser utilisé pour désérialiser les modèles XML. */
+const parser = new XMLParser();
+
+/**
+ * @description Store de l'ensemble des organismes, nécessaire
+ * puisque l'API ne propose aucun endpoint pour en
+ * récupérer une facilement par son identifiant.
+ */
+// const organizations: OrganizationStoreItem[] = [];
+
+let fetchWrapper: FetchCallback = () => new Promise((resolve) => resolve(''));
+
+export function getResponseAsArray<T>(response: T|T[]|undefined): T[] {
+	if (!response) {
+		return []
+	}
+
+	if (typeof response === 'object' && Array.isArray(response)) {
+		return response;
+	}
+
+	return [response];
+}
+
+export function setFetchCallback(callback: FetchCallback) {
+	fetchWrapper = callback;
+}
+
+export function setCredentials(appId: string, appKey: string): void {
+	kAppId = appId;
+	kAppKey = appKey;
+}
+
+export async function callAPI<T, P>(
+	{
+		endpoint,
+		requestParameters = {},
+		normalizationModel,
+		rootKey
+	}: FetchProperties<T,P>
+): Promise<T|T[]|undefined> {
+	try {
+		const response = await makeRequest(endpoint, requestParameters);
+		return deserializeObject<T, P>(response, normalizationModel, rootKey);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error(error.message);
+		} else {
+			console.error(error);
+		}
+		return undefined;
+	}
+}
+
+export function deserializeObject<T, P>(
+	response: string,
+	normalizationModel: Newable<T,P>,
+	rootKey: string
+): T[]|T|undefined {
+	const xml = parser.parse(response) as DecodedXml;
+	delete xml['?xml'];
+
+	const isList = Object.hasOwn(xml, 'liste');
+
+	if (!isList && !Object.hasOwn(xml, rootKey)) {
+		throw new Error('The received response is a single object and does not contain the root key specified.');
+	}
+
+	if (isList && !Object.hasOwn(xml['liste'] as DecodedXml, rootKey)) {
+		throw new Error('The received response is an array of objects, but does not contain the root key specified.');
+	}
+
+	let isSingleResult = true;
+
+	if (isList) {
+		isSingleResult = ((xml['liste'] as DecodedXml)[rootKey] as DecodedXml).length === 1;
+	}
+
+	if (!isList) {
+		return new normalizationModel(xml[rootKey] as unknown as unknown as P);
+	}
+
+	if (isList && isSingleResult) {
+		const entryPath = ((xml['liste'] as DecodedXml)[rootKey] as DecodedXml[])[0] as unknown as P;
+		return new normalizationModel(entryPath);
+	}
+
+	if (isList && !isSingleResult) {
+		const entries = (xml['liste'] as DecodedXml)[rootKey] as DecodedXml[];
+		return entries.map((entry) => new normalizationModel(entry as unknown as P));
+	}
+
+	return undefined;
+}
+
+export async function makeRequest(
+	endpoint: ApiEndpoints,
+	requestParameters: {[key: string]: string} = {}
+): Promise<string> {
+	const time = dayjs().format('YYYYDDMMHHmmss');
+	const sha = new Sha('SHA-1', 'TEXT', {
+		hmacKey: {
+			value: md5(kAppKey),
+			format: 'TEXT',
+			// eslint-disable-next-line unicorn/text-encoding-identifier-case
+			encoding: 'UTF8'
+		},
+		// eslint-disable-next-line unicorn/text-encoding-identifier-case
+		encoding: 'UTF8',
+	});
+	sha.update(time);
+
+	const baseParameters = {
+		serie: kSerial,
+		id: kAppId,
+		tm: time,
+		tmc: sha.getHMAC('HEX'),
+	}
+
+	const parameters = {...baseParameters, ...requestParameters};
+	const searchParameters = new URLSearchParams();
+
+	for (const [key, value] of Object.entries(parameters)) {
+		searchParameters.set(key, String(value));
+	}
+
+	return fetchWrapper(kBaseUrl+endpoint, searchParameters);
+}
+
+// export function mergeRankedAndSpidPlayerCollection(): void {}
+
+// export function populateOrganizations(): void {}
+
+// export function getOrganizationInStore(): void {}
